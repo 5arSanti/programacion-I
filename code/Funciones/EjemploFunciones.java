e895237fbaf1a00962e19c7c58de455da015113c
Santiago Arias <santiari05@hotmail.com>
2024-08-30 13:32:33 +0000

    New Functions class

---
+++ code/Funciones/EjemploFunciones.java
@@ -0,0 +1,15 @@
+public class EjemploFunciones 
+{
+    public static void main(String[] args) 
+    {
+        int a = 20;
+        int b = 30;
+        int result;
+        
+        result = Funciones.Suma(a, b);
+
+        System.out.println(result);
+        
+        Funciones.Multiplicacion(a, b);
+    }
+}
---
+++ code/Funciones/Funciones.java
@@ -0,0 +1,20 @@
+public class Funciones 
+{
+    public static int Suma(int value1, int value2) {
+        
+        value1 = value1 + 5;
+        
+        value2 +=10;
+        
+        return(value1 + value2);
+    }
+    
+    //Procedimiento
+    public static void Multiplicacion(int value1, int value2) 
+    {
+        int result = value1 * value2;
+        
+        System.out.println(result);
+    }
+
+}
